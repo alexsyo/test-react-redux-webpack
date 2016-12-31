a7283986173691e9b9d310a95b663fc0daefe7d9
Alessandro <alex_ok@hotmail.it>
2016-12-31 18:45:41 +0000

    sass config implemented

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: './client/src/Start.js',
   output: {
     filename: 'bundle.js',
-    path: './client/dist/js/'     
+    path: './client/dist/'
   },
   module: {
     loaders: [
       {
         test: /\.js/,
-        loader: "babel-loader",
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel",
         query: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass')
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css', {
+      allChunks: true
+    })
+  ]
 };
